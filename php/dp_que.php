8e9854b34d9c6126eeb1796171d0873514e6f332
Tushar0106 <60386449+Tushar0106@users.noreply.github.com>
2020-03-08 19:32:20 +0530

    Add files via upload

---
+++ php/dp_que.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>DP Question</title>
+        <link href="css/styles.css" rel="stylesheet" />
+        <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+            <!-- top_nav -->
+            <?php require('include/top_nav.php') ?>
+        <div id="layoutSidenav">
+            <div id="layoutSidenav_nav">
+                <!-- nav -->
+                <?php require('include/nav.php') ?>
+            </div>
+            <div id="layoutSidenav_content">
+                <main>
+                    <div class="container-fluid">
+                        <h4 class="mt-4"><u>Desing Pattern</u></h4>
+                       
+                        
+                        <div id="content-box">
+        <div id="post-body">
+            <div class="post-single post-667 page type-page status-publish hentry">
+                
+                <div id="article">
+                    <div class="ead-preview"><div class="ead-document" style="position:relative;padding-top:90%;"><iframe src="files/que/DP.pdf" title="Embedded Document" style="width:100%; height:100%; border: none; position: absolute;left:0;top:0;"></iframe></div><p class="embed_download"><a href="images/17634 2017 Summer Question Paper" download >Download Brochure</a></p></div>
+            
+                </div><!--article-->
+            </div><!--post-single-->
+                        </div><!--post-body-->
+    </div><!--content-box-->
+
+                    </div>
+                </main>
+                <!-- footer -->
+                <?php require('include/footer.php') ?>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="js/scripts.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/chart-area-demo.js"></script>
+        <script src="assets/demo/chart-bar-demo.js"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+        <script src="assets/demo/datatables-demo.js"></script>
+    </body>
+</html>
